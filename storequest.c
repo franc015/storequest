2ebc98ca146ab37f90ef3bb863dedb412dcc122e
Joseph Bartlow <joebartlow@gmail.com>
2013-04-03 10:42:30 -0600

    storequest.c

--- storequest.c
+++ storequest.c
@@ -1,6 +1,15 @@
+/*Store Quest App */
+
+
 #include <stdio.h>
 
 int main (void){
+
+char input_file_name[] = "list_of_products.in";
+
+printf("Welcome to Store Quest. Search an item. If you need help enter (H). If you wish to quit enter (Q).");
+
+ifp =  fopen(input_file_name[], "r");
 
 
 
