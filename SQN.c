ba0ca718f3310e8faeadd10b5540582fd16d8fb3
Eulalia Viana <lali@lali-VirtualBox.(none)>
2013-04-16 16:41:21 -0600

    SQN.c

--- SQN.c
+++ SQN.c
@@ -2,24 +2,45 @@
 #include <string.h>
 
 void instructions(void);
-void d_cmp(char *search_d, char *department[17][9]);	//d = department, cmp = compare
+int d_cmp(char *search_d, char *department[], int num_d);	//d = department, cmp = compare
 
 int main(void)
 {
-	char *search_d;		//d = deprartment
-	char *department[17][9];
+	char search_d[9];		//d = deprartment
+	char department[256][256];
+	char *departments[256];
 	FILE *in;
+	int i=0;
+	int j;
+	int foundIndex;
 
 	in = fopen("product.in", "r");
 
-	fscanf(in, "%s", department[17][9]);
+	while(fscanf(in, "%s", department[i]) != EOF)
+	{
+		departments[i] = department[i];		
+		i++;
+	}
+
+	printf("We scanned %d departments", i);
+	getchar();
+
+	printf("The departments are:\n");
+	for (j = 0; j < i; j++)
+	{
+		printf("\t%s - index: %d\n",department[j], j);
+	}
 
 	instructions();
 
 	printf("Enter Department: ");
 	scanf("%s", search_d);
 
-	d_cmp(search_d, department);
+printf("before d_cmp");
+
+	foundIndex = d_cmp(search_d, departments, i);
+
+	printf("The index number found is %d\n\n",foundIndex);
 
 	fclose(in);
 
@@ -29,14 +50,21 @@ void instructions(void)
 {
 	printf("\nWelcome to Store Quest. \nSearch a department. If you need help enter (H). If you wish to quit enter (Q).\n");
 }
-void d_cmp(char *search_d, char *department[17][9])
+int d_cmp(char *search_d, char *department[], int num_d)
 {
-	if(*search_d == *department[17][9])
+	int i;
+	for(i = 0; i < num_d; i++)
 	{
-		printf("your in a department");
+		if(strcmp(search_d, department[i]) == 0)
+		{
+			printf("\nyour in a department\n");
+			break;
+		}
+		else
+		{
+			printf("\ndepartment does not exisit\n");
+		}
 	}
-	else
-	{
-		printf("department does not exisit");
-	}
+
+	return i;
 }
