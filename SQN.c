e8e7402bade970fa9eac92e3add3ad2a477883b8
Timothy Collinson <collinti@collinti-laptop.(none)>
2013-04-25 22:13:49 -0600

    Added more things

--- SQN.c
+++ SQN.c
@@ -8,24 +8,27 @@ typedef struct{
 }department;
 
 typedef struct{
-	int p_index;
 	char product_name[256];
 	int d_index;
 	int aisle;
 	int shelf;
 	int quantity;
-		double price;
+	double price;
 }product;
 
 void instructions(void);
 int d_cmp(char *search_d, department *departments, int num_d);	//d = department, cmp = compare
 int file_length(char* filename);
 void scan_department(department* departments, char* filename);
+void scan_product(product* products, char* filename);
+void show_prod_by_dept(int department_index, int product_count, product* products);
 
 int main(void)
 {
-	char* filename = "department.in";
-	int department_count = file_length(filename);
+	char* d_filename = "department.in";
+	int department_count = file_length(d_filename);
+	char* p_filename = "product.in";
+	int product_count = file_length(p_filename);
 	char search_d[9];	//d = deprartment
 	int num_d = 0;		//num = number, d = department
 	int j;
@@ -34,9 +37,11 @@ int main(void)
 	instructions();
 
 	department departments[department_count];
-	scan_department(departments, filename);
+	scan_department(departments, d_filename);
+	product products[product_count];
+	scan_product(products, p_filename);
 
-                
+
 	printf("We scanned %d departments", department_count);
 	getchar();
 
@@ -55,6 +60,9 @@ int main(void)
 	if (foundIndex != 17)
 	{
 		printf("The index number found is %d\n\n",foundIndex);
+		printf("Products:\n");
+		show_prod_by_dept(foundIndex, product_count, products);
+		
 	}
 	else
 	{
@@ -87,30 +95,57 @@ int d_cmp(char *search_d, department* departments, int num_d)
 
 int file_length(char* filename){
 
-        FILE *in;
-        in = fopen(filename, "r");
-        int i = 0;
-        char * buffer = malloc(256 * sizeof(char));
-        while((fgets(buffer, 256, in)) != NULL){
-                i++;
-        }
+	FILE *in;
+	in = fopen(filename, "r");
+	int i = 0;
+	char * buffer = malloc(256 * sizeof(char));
+	while((fgets(buffer, 256, in)) != NULL){
+		i++;
+	}
 
-        return i;
+	return i;
 }
 
 void scan_department(department* departments, char* filename){
 
-        FILE *in;
-        in = fopen(filename, "r");
-        int i = 0;
-        char * buffer = malloc(256 * sizeof(char));
-        while((fgets(buffer, 256, in)) != NULL){
-                department *current_department = malloc(sizeof(department));
+	FILE *in;
+	in = fopen(filename, "r");
+	int i = 0;
+	char * buffer = malloc(256 * sizeof(char));
+	while((fgets(buffer, 256, in)) != NULL){
+		department *current_department = malloc(sizeof(department));
 
-                sscanf(buffer, "%d %s", &current_department->d_index, current_department->department_name);
-                departments[i] = *current_department;
-                i++;
-        }
+		sscanf(buffer, "%d %s", &current_department->d_index, current_department->department_name);
+		departments[i] = *current_department;
+		i++;
+	}
 	fclose(in);
 }
 
+void scan_product(product* products, char* filename){
+
+	FILE *in;
+	in = fopen(filename, "r");
+	int i = 0;
+	char * buffer = malloc(256 * sizeof(char));
+	while((fgets(buffer, 256, in)) != NULL){
+		product *current_product = malloc(sizeof(product));
+
+		sscanf(buffer, "%s %d %d %d %d %lf", current_product->product_name, &current_product->d_index, &current_product->aisle, &current_product->shelf, &current_product->quantity, &current_product->price);
+		products[i] = *current_product;
+		i++;
+	}
+	fclose(in);
+}
+
+void show_prod_by_dept(int department_index, int product_count, product* products){
+
+	for(int i = 0; i < product_count; i++){
+
+		if (products[i].d_index == department_index){
+			printf("Product: %s, Aisle Number: %d, Shelf: %d, Quantity: %d, Price: $%.2lf\n", products[i].product_name, products[i].aisle, products[i].shelf, products[i].quantity, products[i].price);
+
+		}
+	}
+
+}
